7dc25d7a763fbab23b861fd6a55cf834309c3f4a
thuy dao <daoduythuy@gmail.com>
2019-01-04 16:18:33 +0700

    Updates

---
+++ precache-manifest.3a757a1d36769b6a3e6a1d128bc0d617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vnasian/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "41a56ad7ad1179578117",
+    "url": "/vnasian/static/js/runtime~main.41a56ad7.js"
+  },
+  {
+    "revision": "2d39da13e9ad3c2254ba",
+    "url": "/vnasian/static/js/main.2d39da13.chunk.js"
+  },
+  {
+    "revision": "323f367ce1732c8e49cf",
+    "url": "/vnasian/static/js/1.323f367c.chunk.js"
+  },
+  {
+    "revision": "2d39da13e9ad3c2254ba",
+    "url": "/vnasian/static/css/main.585f3942.chunk.css"
+  },
+  {
+    "revision": "ced27aa5de3847b1d2fa1fa396c05882",
+    "url": "/vnasian/index.html"
+  }
+];
